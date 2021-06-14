57048e30561eeaa2203498516afcf252ea501f7e
Tai Mujarin <tai.mujarin@gmail.com>
2021-06-14 14:26:18 -0700

    added reservation objects

---
+++ reservations.js
@@ -0,0 +1,23 @@
+const server = require('server');
+
+const reservations = [
+    {
+        id = 'A',
+        name = 'A',
+        email = 'A',
+        phoneNumber = 'A'
+    },
+    {
+        id = 'B',
+        name = 'B',
+        email = 'B',
+        phoneNumber = 'B'
+    },
+    {
+        id = 'C',
+        name = 'C',
+        email = 'C',
+        phoneNumber = 'C'
+    }
+];
+
