39a4daaa8aeaa33940e0390981788d42bc2aac6b
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-06-14 18:02:44 -0700

    Updated tables.html to match html of example

--- routes/pages.js
+++ routes/pages.js
@@ -1,6 +1,6 @@
 // Basic route fo all non-API pages
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
-app.get('/view', (req, res) => res.sendFile(path.join(__dirname, 'view.html')));
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
 
 app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
